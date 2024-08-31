d6c4983b10a630e16f901b7186a7dac91fcc42d4
Pritam12F <pritam.das.santu@gmail.com>
2024-08-31 12:21:07 +0000

    changed button and svg color

--- packages/ui/src/components/navbar.tsx
+++ packages/ui/src/components/navbar.tsx
@@ -20,7 +20,7 @@ export const Navbar = () => {
             {" "}
             <path
               d="M7 12H17M8 8.5C8 8.5 9 9 10 9C11.5 9 12.5 8 14 8C15 8 16 8.5 16 8.5M8 15.5C8 15.5 9 16 10 16C11.5 16 12.5 15 14 15C15 15 16 15.5 16 15.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-              stroke="#CCC9DC"
+              stroke="#e5e5e5"
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -28,7 +28,7 @@ export const Navbar = () => {
           </g>
         </svg>
       </div>
-      <Button variant={"secondary"} className="w-32 mx-3 bg-customgray-400">
+      <Button variant={"secondary"} className="w-32 mx-3 bg-neutral-200">
         Log in
       </Button>
     </nav>
